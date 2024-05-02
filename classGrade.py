f12bcee7013b29248916ea46c6172843a3e433f4
Matthew Noah <mnoah@unomaha.edu>
2024-05-02 15:42:53 -0500

    current version / regressions added

--- classGrade.py
+++ classGrade.py
@@ -1,59 +1,273 @@
 import tkinter as tk
+import warnings
 from tkinter import ttk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from tkinter import filedialog  
+import pandas as pd  
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
+
+
+warnings.filterwarnings("ignore", message="X does not have valid feature names")
 years = []
 grades = []
 descriptions = []
 modality = []  
+classtype = []
+imported = False
 
-# Function to update the graph and best-fit line
+OnlineClassFil = True 
+InPersonClassFil = True 
+appFil = True
+theoryFil = True
+linearReg = False
+nameToggle = True
+randomForestToggle =  False
+multiRegress = False
+
+def toggle_online_class():
+    global OnlineClassFil
+    OnlineClassFil = not OnlineClassFil
+    plot_graph()
+    
+def toggle_inperson_class():
+    global InPersonClassFil
+    InPersonClassFil = not InPersonClassFil
+    plot_graph()
+    
+def toggle_app():
+    global appFil
+    appFil = not appFil
+    plot_graph()
+    
+def toggle_theory():
+    global theoryFil
+    theoryFil = not theoryFil
+    plot_graph()
+
+def toggle_regress():
+    global linearReg
+    linearReg = not linearReg
+    plot_graph()
+
+def toggle_name():
+    global nameToggle
+    nameToggle = not nameToggle
+    plot_graph()
+
+def toggle_spline():
+    global randomForestToggle
+    randomForestToggle = not randomForestToggle
+    plot_graph()
+    
+def toggle_multiRegress():
+    global multiRegress
+    multiRegress = not multiRegress
+    plot_graph()
+    
 def update_graph():
-    year = int(year_var.get())
-    grade = float(grade_entry.get())
-    description = description_entry.get()
-    class_type = class_type_var.get()
+    global imported 
+    if not imported:
+        year = int(year_var.get())
+        grade = float(grade_entry.get())
+        description = description_entry.get()
+        class_mod = class_type_var.get()
+        class_type = class_type_var1.get()
+    
+        if year and grade:
+            years.append(year)
+            grades.append(grade)
+            descriptions.append(description)
+            modality.append(class_mod)
+            classtype.append(class_type)
+            ax.clear()
+            plot_graph()
+    else:
+        plot_graph()
 
-    if year and grade:
-        years.append(year)
-        grades.append(grade)
-        descriptions.append(description)
-        modality.append(class_type)
-        ax.clear()
-
+def plot_graph():
         # Separate data points into online and in-person classes
-        onlineClass = [years[i] for i in range(len(years)) if modality[i] == 1]
-        onlineGrade = [grades[i] for i in range(len(years)) if modality[i] == 1]
-        inPerson = [years[i] for i in range(len(years)) if modality[i] == 0]
-        inPersonGrade = [grades[i] for i in range(len(years)) if modality[i] == 0]
-
-        # Plot online and in-person classes as orange and blue dots respectively
-        ax.scatter(onlineClass, onlineGrade, marker='o', s=100, label='Online Class', color='orange')
-        ax.scatter(inPerson, inPersonGrade, marker='o', s=100, label='In-Person Class', color='blue')
-
-        # Calculate and plot the best-fit lines based on class modality
-        if len(onlineClass) > 1:
-            online_coefficients = np.polyfit(onlineClass, onlineGrade, 1)
-            online_best_fit_line = np.poly1d(online_coefficients)
-            ax.plot(onlineClass, online_best_fit_line(onlineClass), color='orange', linestyle='--', label='Online Class Best Fit')
-
-        if len(inPerson) > 1:
-            in_person_coefficients = np.polyfit(inPerson, inPersonGrade, 1)
-            in_person_best_fit_line = np.poly1d(in_person_coefficients)
-            ax.plot(inPerson, in_person_best_fit_line(inPerson), color='blue', linestyle='--', label='In-Person Class Best Fit')
+        yearsTemp = years.copy()
+        gradesTemp = grades.copy()
+        descriptionsTemp = descriptions.copy()
+        descriptionsGradeTemp = grades.copy()
+        modalityTemp = modality.copy()
+        classtypeTemp = classtype.copy()
         
-        combYears = years
-        combGrades = grades
-        if len(combYears) > 1:
-            combined_coefficients = np.polyfit(combYears, combGrades, 1)
-            combined_best_fit_line = np.poly1d(combined_coefficients)
-            ax.plot(combYears, combined_best_fit_line(combYears), color='green', linestyle='--', label='Combined Best Fit')
+        # sort data based on year asc
+        tuple = list(zip(yearsTemp, gradesTemp, descriptionsTemp, descriptionsGradeTemp, modalityTemp, classtypeTemp))
+        sort = sorted(tuple, key=lambda x: x[0])
+        yearsTemp = [pair[0] for pair in sort]
+        gradesTemp = [pair[1] for pair in sort]
+        descriptionsTemp = [pair[2] for pair in sort]
+        descriptionsGradeTemp = [pair[3] for pair in sort]
+        modalityTemp = [pair[4] for pair in sort]
+        classtypeTemp = [pair[5] for pair in sort]
+        
+        # remove online classes
+        if not OnlineClassFil:
+            for i in range(len(modalityTemp) - 1, -1, -1):
+                if modalityTemp[i] == 1:
+                    del yearsTemp[i]
+                    del gradesTemp[i]
+                    del descriptionsTemp[i]
+                    del descriptionsGradeTemp[i]
+                    del modalityTemp[i]
+                    del classtypeTemp[i]
+        # remove inperson classes
+        if not InPersonClassFil:
+            for i in range(len(modalityTemp) - 1, -1, -1):
+                if modalityTemp[i] == 0:
+                    del yearsTemp[i]
+                    del gradesTemp[i]
+                    del descriptionsTemp[i]
+                    del descriptionsGradeTemp[i]
+                    del modalityTemp[i]
+                    del classtypeTemp[i]
+        # remove application type
+        if not appFil:
+            for i in range(len(classtypeTemp) - 1, -1, -1):
+                if classtypeTemp[i] == 1:
+                    del yearsTemp[i]
+                    del gradesTemp[i]
+                    del descriptionsTemp[i]
+                    del descriptionsGradeTemp[i]
+                    del modalityTemp[i]
+                    del classtypeTemp[i]
+         # remove theory type
+        if not theoryFil:
+            for i in range(len(classtypeTemp) - 1, -1, -1):
+                if classtypeTemp[i] == 0:
+                    del yearsTemp[i]
+                    del gradesTemp[i]
+                    del descriptionsTemp[i]
+                    del descriptionsGradeTemp[i]
+                    del modalityTemp[i]
+                    del classtypeTemp[i]
+        
+        
+        ax.clear()
+        if linearReg and len(gradesTemp) > 0:
+            
+            dates = np.array(yearsTemp)
+            grade = np.array(gradesTemp)
 
-        # Annotate points with descriptions
-        for x, y, desc in zip(years, grades, descriptions):
-            ax.annotate(desc, (x, y), fontsize=12, ha='center', va='bottom')
+            X = dates.reshape(-1, 1)
+            model = LinearRegression()
+            model.fit(X, grade)
+            predicted_grade = model.predict(X)
+
+            # Calculate RMSE
+            rmse = round(np.sqrt(mean_squared_error(grade, predicted_grade)), 3)
+            # Predict grades for the next 5 years
+            greatDate = dates[len(dates) - 1]
+            next_years = np.array([greatDate  + 1, greatDate  + 2 , greatDate  + 3, greatDate+ 4, greatDate + 5]).reshape(-1, 1)
+            predicted_grades = model.predict(next_years)
+            textValue = "Predicted Grades : RMSE = "+str(rmse)
+            
+            for i in range(len(predicted_grades)):
+                if predicted_grades[i] > 100:
+                    predicted_grades[i] = 100
+            ax.scatter(next_years, predicted_grades, marker='o', s=100, label=textValue, color='red')
+            ax.scatter(yearsTemp, gradesTemp, marker='o', s=100, color='blue')
+            for x, y, predicted_grades in zip(next_years, predicted_grades, predicted_grades):
+                ax.annotate(str(round(predicted_grades, 1)), (x, y), fontsize=12, ha='center', va='bottom')
+            if len(yearsTemp) > 1:
+                classesBestFit = np.polyfit(yearsTemp, gradesTemp, 1)
+                bestFitLine = np.poly1d(classesBestFit)
+                ax.plot(yearsTemp, bestFitLine(yearsTemp), color='red', linestyle='--', label='Online Class Best Fit')
+                
+
+            
+        if randomForestToggle:
+            if OnlineClassFil and InPersonClassFil or appFil and theoryFil:
+                ax.scatter([], [], marker='o', s=100, label="Random Forest: Select only 1 modality and type field", color='orange')
+                
+            elif (OnlineClassFil or InPersonClassFil) and (appFil or theoryFil):      
+                X = list(zip(years, modality, classtype))
+                y = grades
+
+
+                X_train = np.column_stack((years, modality, classtype))
+                y_train = grades
+
+
+                rf_regressor = RandomForestRegressor(n_estimators=15, random_state=42)
+                rf_regressor.fit(X_train, y_train)
+
+
+                future_years = np.array([max(years)+1, max(years)+2])  
+                
+                
+                mode = 0
+                type = 0
+                if appFil:
+                    mode = 1
+                if OnlineClassFil:
+                    type = 1
+                next_years = np.array([[max(years) + 1, mode, type], [max(years) + 2, mode, type]])
+                
+                future_modality = np.array([mode, mode])  
+                future_classtype = np.array([type, type])  
+
+                X_test = np.column_stack((future_years, future_modality, future_classtype))
+                predicted_grades = rf_regressor.predict(X_test)
+
+                # Print predicted grades for the next 5 years
+                for i in range(len(predicted_grades)):
+                    if predicted_grades[i] > 100:
+                        predicted_grades[i] = 100
+                ax.scatter(future_years, predicted_grades, marker='o', s=100, label="Random Forest Prediction", color='orange')
+                for x, y, predicted_grades in zip(future_years, predicted_grades, predicted_grades):
+                    ax.annotate(str(round(predicted_grades,1)), (x, y), fontsize=12, ha='center', va='bottom')
+
+        
+        if multiRegress:
+            if OnlineClassFil and InPersonClassFil or appFil and theoryFil:
+                ax.scatter([], [], marker='o', s=100, label="Multi-Regression: Select only 1 modality and type field", color='green')
+                
+            elif (OnlineClassFil or InPersonClassFil) and (appFil or theoryFil):
+                data = pd.DataFrame({'grades': grades, 'year': years, 'modality': modality, 'classtype': classtype})
+                X = data[['year', 'modality', 'classtype']]  
+                y = data['grades']  
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+                model = LinearRegression()
+                model.fit(X_train, y_train)
+
+                mode = 0
+                type = 0
+                if appFil:
+                    mode = 1
+                if OnlineClassFil:
+                    type = 1
+                next_years = np.array([[max(years) + 1, mode, type], [max(years) + 2, mode, type], [max(years) + 3, mode, type], [max(years) + 4, mode, type], [max(years) + 5, mode, type]])
+                predictions = model.predict(next_years)
+                ypredicted = model.predict(X_test)
+                rmse = np.sqrt(mean_squared_error(y_test, ypredicted))
+                textValue = "Multi-Regression: RMSE = "+str(rmse)
+                next_years1 = [max(years) + 1, max(years) + 2,max(years) + 3,max(years) + 4,max(years) + 5]
+                for i in range(len(predictions)):
+                    if predictions[i] > 100:
+                        predictions[i] = 100
+                ax.scatter(next_years1, predictions, marker='o', s=100, label=textValue, color='green')
+                for x, y, prediction in zip(next_years1, predictions, predictions):
+                    ax.annotate(str(round(prediction,1)), (x, y), fontsize=12, ha='center', va='bottom')
+                            
+            
+        ax.scatter(yearsTemp, gradesTemp, marker='o', s=100, label='Class Grade', color='blue')
+
+        if nameToggle:
+            for x, y, desc in zip(yearsTemp, gradesTemp, descriptionsTemp):
+                ax.annotate(desc, (x, y), fontsize=12, ha='center', va='bottom')
+        else: 
+            for x, y, desc in zip(yearsTemp, gradesTemp, []):
+                ax.annotate(desc, (x, y), fontsize=12, ha='center', va='bottom')   
 
         ax.set_title("Class Percentage Grades Over the Years")
         ax.set_xlabel("Year")
@@ -61,16 +275,46 @@ def update_graph():
         ax.grid(True)
         ax.legend()
 
-        ax.set_xlim(2005, 2024)
+        ax.set_xlim(2002, 2028)
         ax.set_ylim(50, 105)
         ax.xaxis.set_major_locator(plt.MaxNLocator(integer=True))
 
         # Redraw the canvas
         canvas.draw()
-    print(years)
-    print(descriptions)
-    print(grades)
-    print(modality)
+ 
+ 
+def import_data():
+    global years 
+    global grades 
+    global descriptions 
+    global modality   
+    global classtype   
+    years = []
+    grades = []
+    descriptions = []
+    modality = []
+    classtype = []
+    import_data1()
+    
+def import_data1():
+    global imported  
+    imported = True
+    file_path = filedialog.askopenfilename(filetypes=[("Excel Files", "*.xlsx")])
+    if file_path:
+        # Read file and grab data
+        df = pd.read_excel(file_path)
+        years.extend(df['year'].tolist())
+        grades.extend(df['grade'].tolist())
+        descriptions.extend(df['name'].tolist())
+        
+        global modality  
+        modality.extend(df['modality'].tolist())
+        modality = [0 if mode == 'in person' else 1 for mode in modality]
+        
+        global classtype 
+        classtype.extend(df['application'].tolist())
+        classtype = [1 if type1 == 'application' else 0 for type1 in classtype]
+        update_graph()
 
 # Create the main application window
 root = tk.Tk()
@@ -97,7 +341,7 @@ description_entry.grid(row=2, column=1, padx=5, pady=5)
 description_entries.append(description_entry)
 
 # Class modality selection
-ttk.Label(data_frame, text="Class Type:").grid(row=3, column=0, padx=5, pady=5)
+ttk.Label(data_frame, text="Class Modality:").grid(row=3, column=0, padx=5, pady=5)
 class_type_var = tk.IntVar()
 class_type_var.set(0)  
 online_radio = ttk.Radiobutton(data_frame, text="Online", variable=class_type_var, value=1)
@@ -105,13 +349,61 @@ online_radio.grid(row=3, column=1, padx=5, pady=5)
 in_person_radio = ttk.Radiobutton(data_frame, text="In-Person", variable=class_type_var, value=0)
 in_person_radio.grid(row=3, column=2, padx=5, pady=5)
 
+# Academic / Thoery Based
+ttk.Label(data_frame, text="Class Type:").grid(row=3, column=0, padx=5, pady=5)
+class_type_var1 = tk.IntVar()
+class_type_var1.set(0)  
+online_radio = ttk.Radiobutton(data_frame, text="Application", variable=class_type_var1, value=1)
+online_radio.grid(row=4, column=1, padx=5, pady=5)
+in_person_radio = ttk.Radiobutton(data_frame, text="Theory", variable=class_type_var1, value=0)
+in_person_radio.grid(row=4, column=2, padx=5, pady=5)
+
 # Create a button to add data and update the graph
 add_button = ttk.Button(data_frame, text="Add Data", command=update_graph)
-add_button.grid(row=4, columnspan=2, padx=5, pady=10)
+add_button.grid(row=5, columnspan=2, padx=5, pady=10)
 
-# Create a frame for displaying the graph
+import_button = ttk.Button(data_frame, text="Import Data", command=import_data)
+import_button.grid(row=7, columnspan=2, padx=5, pady=10)
+
+
+# GUI
 graph_frame = ttk.Frame(root)
 graph_frame.pack(padx=20, pady=20)
+
+online_checkbox_var = tk.BooleanVar(value=OnlineClassFil)
+online_checkbox = ttk.Checkbutton(data_frame, text="Online", variable=online_checkbox_var, command=toggle_online_class)
+online_checkbox.grid(row=8, column=1, padx=5, pady=10)
+
+inperson_checkbox_var = tk.BooleanVar(value=InPersonClassFil)
+inperson_checkbox = ttk.Checkbutton(data_frame, text="In-Person", variable=inperson_checkbox_var, command=toggle_inperson_class)
+inperson_checkbox.grid(row=8, column=2, padx=5, pady=10)
+
+inperson_checkbox_var = tk.BooleanVar(value=appFil)
+inperson_checkbox = ttk.Checkbutton(data_frame, text="Application", variable=inperson_checkbox_var, command=toggle_app)
+inperson_checkbox.grid(row=8, column=3, padx=5, pady=10)
+
+inperson_checkbox_var = tk.BooleanVar(value=theoryFil)
+inperson_checkbox = ttk.Checkbutton(data_frame, text="Theory", variable=inperson_checkbox_var, command=toggle_theory)
+inperson_checkbox.grid(row=8, column=5, padx=5, pady=10)
+
+
+inperson_checkbox_var = tk.BooleanVar(value=nameToggle)
+inperson_checkbox = ttk.Checkbutton(data_frame, text="Name / Grade", variable=inperson_checkbox_var, command=toggle_name)
+inperson_checkbox.grid(row=8, column=7, padx=5, pady=10)
+
+# linear regress
+linearReg_var = tk.BooleanVar(value=linearReg)
+linearReg_checkbox = ttk.Checkbutton(data_frame, text="Best-Fit", variable=linearReg_var, command=toggle_regress)
+linearReg_checkbox.grid(row=9, column=1, padx=5, pady=10)
+
+
+spline_var = tk.BooleanVar(value=randomForestToggle)
+spline_checkbox = ttk.Checkbutton(data_frame, text="Random Forest", variable=spline_var, command=toggle_spline)
+spline_checkbox.grid(row=9, column=3, padx=5, pady=10)
+
+multiReg_var = tk.BooleanVar(value=multiRegress)
+multiReg_checkbox = ttk.Checkbutton(data_frame, text="Multi-Regression", variable=multiReg_var, command=toggle_multiRegress)
+multiReg_checkbox.grid(row=9, column=2, padx=5, pady=10)
 
 # Create a new 2D scatter plot
 fig, ax = plt.subplots(figsize=(8, 6))
@@ -119,9 +411,8 @@ ax.set_title("Class Percentage Grades Over the Years")
 ax.set_xlabel("Year")
 ax.set_ylabel("Percentage Grade")
 ax.grid(True)
-ax.set_xlim(2005, 2024)
+ax.set_xlim(2002, 2028)
 ax.set_ylim(50, 105)
-# Set the x-axis to display only whole years
 ax.xaxis.set_major_locator(plt.MaxNLocator(integer=True))
 
 canvas = FigureCanvasTkAgg(fig, master=graph_frame)
--- dataGenerator.py
+++ dataGenerator.py
@@ -1,59 +1,124 @@
 import pandas as pd
 import random
 
+
+
+onlineDiff = False
+onlineDelta = 6
+
+inpersonDiff = True
+inpersonDelta = 4
+
+applicationDiff = False
+applicationDelta = 5  #value between -10 and 8
+
+theoryDiff = True
+theoryDelta = -10
+
+increasing = True
+factor = 0.1 # use value between -0.1 and 0.15
+factorshift = 0.05
+
+
+
+
+
 # List of classes
 classes = [
     {"name": "CIST 1400", "description": "INTRODUCTION TO COMPUTER SCIENCE I", "application": "application"},
     {"name": "CSCI 1620", "description": "INTRODUCTION TO COMPUTER SCIENCE II", "application": "application"},
     {"name": "CSCI 2240", "description": "INTRODUCTION TO C PROGRAMMING", "application": "application"},
-    {"name": "CIST 2100", "description": "ORGANIZATIONS, APPLICATIONS AND TECHNOLOGY", "application": "neither"},
-    {"name": "CIST 3110", "description": "INFORMATION TECHNOLOGY ETHICS", "application": "application"},
-    {"name": "MATH 1950", "description": "CALCULUS", "application": ""},
-    {"name": "CSCI 2030", "description": "MATHEMATICAL FOUNDATIONS OF COMPUTER SCIENCE", "application": ""},
-    {"name": "CSCI 2040", "description": "INTRODUCTION TO MATHEMATICAL PROOFS", "application": ""},
-    {"name": "MATH 2050", "description": "APPLIED LINEAR ALGEBRA", "application": ""},
-    {"name": "CIST 2500", "description": "INTRODUCTION TO APPLIED STATISTICS FOR IS&T", "application": ""},
-    {"name": "CSCI 3320", "description": "DATA STRUCTURES", "application": ""},
-    {"name": "CSCI 3550", "description": "COMMUNICATION NETWORKS", "application": ""},
-    {"name": "CSCI 3660", "description": "THEORY OF COMPUTATION", "application": "THEORY"},
-    {"name": "CSCI 3710", "description": "INTRODUCTION TO DIGITAL DESIGN AND COMPUTER ORGANIZATION", "application": "THEORY"},
-    {"name": "CSCI 4100", "description": "INTRODUCTION TO ALGORITHMS", "application": "THEORY"},
-    {"name": "CSCI 4220", "description": "PRINCIPLES OF PROGRAMMING LANGUAGES", "application": "THEORY"},
+    {"name": "CIST 2100", "description": "ORGANIZATIONS, APPLICATIONS AND TECHNOLOGY", "application": "theory"},
+    {"name": "CIST 3110", "description": "INFORMATION TECHNOLOGY ETHICS", "application": "theory"},
+    {"name": "MATH 1950", "description": "CALCULUS", "application": "theory"},
+    {"name": "CSCI 2030", "description": "MATHEMATICAL FOUNDATIONS OF COMPUTER SCIENCE", "application": "theory"},
+    {"name": "CSCI 2040", "description": "INTRODUCTION TO MATHEMATICAL PROOFS", "application": "theory"},
+    {"name": "MATH 2050", "description": "APPLIED LINEAR ALGEBRA", "application": "theory"},
+    {"name": "CIST 2500", "description": "INTRODUCTION TO APPLIED STATISTICS FOR IS&T", "application": "theory"},
+    {"name": "CSCI 3320", "description": "DATA STRUCTURES", "application": "application"},
+    {"name": "CSCI 3550", "description": "COMMUNICATION NETWORKS", "application": "application"},
+    {"name": "CSCI 3660", "description": "THEORY OF COMPUTATION", "application": "theory"},
+    {"name": "CSCI 3710", "description": "INTRODUCTION TO DIGITAL DESIGN AND COMPUTER ORGANIZATION", "application": "theory"},
+    {"name": "CSCI 4100", "description": "INTRODUCTION TO ALGORITHMS", "application": "theory"},
+    {"name": "CSCI 4220", "description": "PRINCIPLES OF PROGRAMMING LANGUAGES", "application": "theory"},
     {"name": "CSCI 4350", "description": "COMPUTER ARCHITECTURE", "application": "application"},
     {"name": "CSCI 4500", "description": "OPERATING SYSTEMS", "application": ""},
-    {"name": "CSCI 4830", "description": "INTRODUCTION SOFTWARE ENGINEERING", "application": ""},
-    {"name": "CSCI 4970", "description": "CAPSTONE PROJECT", "application": ""},
-    {"name": "CSCI 4000", "description": "ASSESSMENT", "application": ""}
+    {"name": "CSCI 4830", "description": "INTRODUCTION SOFTWARE ENGINEERING", "application": "application"},
+    {"name": "CSCI 4970", "description": "CAPSTONE PROJECT", "application": "application"},
+    {"name": "CSCI 4000", "description": "ASSESSMENT", "application": "application"},
+    
+    {"name": "CSCI 3000", "description": "elective", "application": "application"},
+    {"name": "CSCI 3010", "description": "elective", "application": "theory"},
+    {"name": "CSCI 3100", "description": "Ielective", "application": "theory"},
+    {"name": "CSCI 3900", "description": "elective", "application": "theory"},
+    {"name": "CSCI 3800", "description": "elective", "application": "theory"},
+    {"name": "CSCI 3400", "description": "elective", "application": "application"},
+    {"name": "CSCI 3610", "description": "elective", "application": "theory"},
+    {"name": "CSCI 4050", "description": "elective", "application": "application"},
+    {"name": "CSCI 4110", "description": "elective", "application": "application"},
+    {"name": "CSCI 4660", "description": "elective", "application": "application"},
+    
+    {"name": "CSCI 2050", "description": "elective", "application": "application"},
+    {"name": "CSCI 2090", "description": "elective", "application": "theory"},
+    {"name": "CSCI 2110", "description": "Ielective", "application": "theory"},
+    {"name": "CSCI 2550", "description": "elective", "application": "theory"},
+    {"name": "CSCI 2600", "description": "elective", "application": "theory"},
+    {"name": "CSCI 2900", "description": "elective", "application": "application"},
+    {"name": "CSCI 2260", "description": "elective", "application": "theory"},
+    {"name": "CSCI 4310", "description": "elective", "application": "application"},
+    {"name": "CSCI 4390", "description": "elective", "application": "application"},
+    {"name": "MATH 4660", "description": "elective", "application": "application"}
 ]
 
-# data columns
 df = pd.DataFrame(columns=["year", "name", "description", "application", "modality", "grade"])
 
 # dictionary to keep track of scheduled classes for each year
-scheduled_classes = {year: set() for year in range(2000, 2023)}
+scheduled_classes = {year: set() for year in range(2005, 2020)}
 
 # Generate the class schedule a range of years
-for year in range(2000, 2023):
+for year in range(2005, 2020):
     year_data = []
-    for _ in range(10):
-        # choose a class that is not repeated 
+    for _ in range(15):
+        # choose a class
         available_classes = [c for c in classes if c["name"] not in scheduled_classes[year]]
         if not available_classes:
             break  
         class_data = random.choice(available_classes)
+        modality = random.choice(["online", "in person"])
+        application = class_data["application"]
+        grade = 80 
+        
+        
+
+        normalDis = random.normalvariate(0, 2)*4
+        grade = grade + normalDis
+        if onlineDiff and modality == "online":
+           grade = grade + random.uniform(0.3, 1) * onlineDelta 
+        elif inpersonDiff and modality == "in person":
+            grade = grade + random.uniform(0.3, 1) * inpersonDelta
+        if applicationDiff and application == 'application':
+           grade = grade + random.uniform(0.3, 1)* onlineDelta
+        elif theoryDiff and application == 'theory':
+           grade = grade + random.uniform(0.3, 1) * theoryDelta  
+        if increasing:
+            grade = grade + random.random() * factor
+            factor = factor + factorshift
+        
+            
+        if grade > 100:
+            grade = 100
         year_class = {
             "year": year,
             "name": class_data["name"],
             "description": class_data["description"],
             "application": class_data["application"],
-            "modality": random.choice(["online", "in person"]),
-            "grade": random.randint(50, 100)
+            "modality": modality,
+            "grade": grade
         }
         year_data.append(year_class)
         # Update the set of scheduled classes for this year
         scheduled_classes[year].add(class_data["name"])
     df = pd.concat([df, pd.DataFrame(year_data)], ignore_index=True)
-    # df = pd.concat([df, pd.DataFrame({col: [''] for col in df.columns})], ignore_index=True)
 
 # Save the excel
-df.to_excel("class_schedule.xlsx", index=False, sheet_name="testData")
+df.to_excel("test.xlsx", index=False, sheet_name="testData")
